470314c857984d9f7ad37e8dda09547a75da38ff
Gabriela <gabriela_silva21@estudante.sesisenai.org.br>
2025-11-04 10:11:42 -0300

    add - arrumei o que estava errado

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $pass = 'root'; 
-    $db = 'kanban_app';
+    $db = 'industria_alimenticia';
     $conn = new mysqli($host, $user, $pass, $db);
     if ($conn->connect_error) {
         die('Erro de conexão: ' . $conn->connect_error);
